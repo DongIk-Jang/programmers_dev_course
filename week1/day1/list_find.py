1117fed61e1b8d76f1e9d00444d0a73b28496b87
DongIk-Jang <jdandy14@gmail.com>
2021-04-20 13:10:24 +0900

    list - find - complete

---
+++ week1/day1/list_find.py
@@ -0,0 +1,18 @@
+def solution(L, x):
+    counter = 0
+    answer = []
+    if x in L:
+        for i in L:
+            if i == x:
+                answer.append(counter)
+                counter += 1
+            else:
+                counter += 1
+    else:
+        answer = [-1]
+    
+    return answer
+
+L = [64,72,83,72,54]
+x = 72
+print(solution(L, x))
